87508235acf234850e6a0c51005add5abe6ab8f9
imronxz <imronxz@gmail.com>
2022-09-15 23:57:29 +0700

    Fungsi Pada Promises

--- promise.js
+++ promise.js
@@ -1,8 +1,24 @@
-const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
+const { promiseTheaterIXX, promiseTheaterVGC } = require('./external.js');
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (hasilPenonton) => {
+  let hasil = 0;
+  const hasilIxx = await promiseTheaterIXX(hasilPenonton);
+  const hasilVGC = await promiseTheaterVGC(hasilPenonton);
 
+  // Perulangan
+  for (const element of hasilIxx) {
+    if (hasilPenonton === element.hasil) {
+      hasil++;
+    }
+  }
+  for (const element of hasilVGC) {
+    if (hasilPenonton === element.hasil) {
+      hasil++;
+    }
+  }
+  return hasil
+};
 module.exports = {
   promiseOutput,
 };
